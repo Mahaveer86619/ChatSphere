031141f4e8f5b81f961160446042b307c9245b2f
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-07-28 18:24:22 +0530

    Made ui for SignIn Screen and logIn Screen

--- app/src/main/java/com/sev7en/chatsphere/Screens/LoginScreen.kt
+++ app/src/main/java/com/sev7en/chatsphere/Screens/LoginScreen.kt
@@ -1,17 +1,25 @@
 package com.sev7en.chatsphere.Screens
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.sev7en.chatsphere.R
+import com.sev7en.chatsphere.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
-    }
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    fun ShowHidePass(view: View) {
+        binding.tvToSignIn.setOnClickListener {
+            startActivity(Intent(this, SignInScreen::class.java))
+            finish()
+        }
 
     }
 }
---
+++ app/src/main/java/com/sev7en/chatsphere/Screens/SignInScreen.kt
@@ -0,0 +1,22 @@
+package com.sev7en.chatsphere.Screens
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sev7en.chatsphere.databinding.ActivitySignInScreenBinding
+
+class SignInScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvToLogIn.setOnClickListener {
+            startActivity(Intent(this, LoginScreen::class.java))
+            finish()
+        }
+    }
+}
