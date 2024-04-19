960b623ab426b02ee0a13fa3ed6a2e754912f6a0
Mahaveer <psatyam86619@gmail.com>
2024-04-19 14:38:59 +0530

    Connected Firebase and Chaneged App Icon

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ChatSphere',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      home: const SafeArea(
+        child: Placeholder(),
+      ),
     );
   }
 }
